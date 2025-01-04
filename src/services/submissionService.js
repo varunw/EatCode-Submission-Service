7bc0b6568687a151f586ee3f933f9c1c5efd7d41
varun waykar <waykarvarun@gmail.com>
2025-01-04 21:08:39 +0530

    Added sync communication between microservices

--- src/services/submissionService.js
+++ src/services/submissionService.js
@@ -1,3 +1,4 @@
+const { fetchProblemDetails } = require('../apis/problemAdminApi');
 const SubmissionProducer = require('../producers/submissionQueueProducer')
 
 class SubmissionService{
@@ -10,11 +11,27 @@ class SubmissionService{
     }
 
     async addSubmission(submissionPayload){
+        const problemId = submissionPayload.problemId;
+        const problemAdminApiResponse = await fetchProblemDetails(problemId);
+        if(!problemAdminApiResponse){
+            throw{message:"Not able to create submission"};
+        }
+        console.log(problemAdminApiResponse);
+        const languageCodeStub = problemAdminApiResponse.data.codeStub.find(codeStub=>codeStub.language.toLowerCase()==submissionPayload.language.toLowerCase());
+        submissionPayload.code = languageCodeStub.startSnippet +"\n\n" + submissionPayload.code + "\n\n" + languageCodeStub.endSnippet;
+        
         const submission = await this.submissionRepository.addSubmission(submissionPayload);
         if(!submission){
             throw{message:"Not able to create submission"};
         }
-        const response = await SubmissionProducer(submission);
+        const response = await SubmissionProducer({
+            [submission._id]:{
+                code :submission.code,
+                language:submission.language,
+                inputCase:problemAdminApiResponse.data.testCases[0].input,
+                outputCase:problemAdminApiResponse.data.testCases[0].output,
+            }
+        });
         return {queueResponse:response,submission};
     }
 }
